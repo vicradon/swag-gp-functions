1f8061913333c4f46965e4576c747a5d57bb8ddd
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-10-18 09:12:39 +0100

    chore: add email validation to functions

--- login/index.js
+++ login/index.js
@@ -1,14 +1,18 @@
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
   try {
     const { query: q, Client } = require("faunadb");
 
     const client = new Client({
       secret: process.env.DB_SECRET,
     });
+    const { email, password } = req.body;
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      throw new Error("Invalid email");
+    }
     const credentials = await client.query(
       q.Let(
         {
-          user: q.Get(q.Match(q.Index("users_by_email"), req.body.email)),
+          user: q.Get(q.Match(q.Index("users_by_email"), email)),
         },
         {
           status: "success",
@@ -16,12 +20,12 @@ module.exports = async function(context, req) {
           secret: q.Select(
             ["secret"],
             q.Login(q.Select(["ref"], q.Var("user")), {
-              password: req.body.password,
+              password,
               ttl: q.TimeAdd(q.Now(), 1, "day"),
-            }),
+            })
           ),
-        },
-      ),
+        }
+      )
     );
     context.res = {
       status: 200,
--- signup/index.js
+++ signup/index.js
@@ -1,4 +1,4 @@
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
   try {
     const { query: q, Client } = require("faunadb");
 
@@ -6,6 +6,11 @@ module.exports = async function(context, req) {
       secret: process.env.DB_SECRET,
     });
     const { firstName, lastName, email, password } = req.body;
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      throw new Error("Invalid email");
+    }
+
     const credentials = await client.query(
       q.Let(
         {
@@ -27,11 +32,11 @@ module.exports = async function(context, req) {
               q.Login(q.Select(["ref"], q.Var("user")), {
                 password: req.body.password,
                 ttl: q.TimeAdd(q.Now(), 1, "day"),
-              }),
+              })
             ),
           },
-        },
-      ),
+        }
+      )
     );
     context.res = {
       status: 200,
